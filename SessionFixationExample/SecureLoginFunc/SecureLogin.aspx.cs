2210e153f00917a108e460386016c49eb298951d
haricharan <haricharan.konduri@raybiztech.com>
2017-07-31 12:55:26 +0530

    Added Insecure and Secure Login Mechanisms

---
+++ SessionFixationExample/SecureLoginFunc/SecureLogin.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SessionFixationExample
+{
+    public partial class SecureLogin : System.Web.UI.Page
+    {
+        //This is the page load event. 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblMessage.ForeColor = System.Drawing.Color.DarkKhaki;
+            lblMessage.Text = " username and password: [admin/admin], [test/test] ";
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //When user enter valid credentials in username and password field
+            if ((txtUsername.Text.Trim().Equals("admin") && txtPassword.Text.Trim().Equals("admin")) ||
+               (txtUsername.Text.Trim().Equals("test") && txtPassword.Text.Trim().Equals("test")))
+            {
+                //Creating a Session for that user
+                Session["userLoggedin"] = txtUsername.Text.Trim();
+
+                string guid = Guid.NewGuid().ToString();
+                //Creating second session for the same user and assigning a randmon GUID
+                Session["AuthToken"] = guid;
+                
+                //Creating cookie and storing the same value of second session in the cookie
+                Response.Cookies.Add(new HttpCookie("AuthToken", guid));
+
+                //Redirecting user to Logout Page
+                Response.Redirect("~/SecureLoginFunc/SecureLogout.aspx");
+            }
+            else
+            {
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Text = "Invalid username or password";
+            }
+        }
+    }
+}
---
+++ SessionFixationExample/SecureLoginFunc/SecureLogout.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using System.Web.UI.WebControls;
+
+namespace SessionFixationExample
+{
+    public partial class SecureLogout : System.Web.UI.Page
+    {
+        //This is page load event of Logout Page, this event will be triggered when user redirected to this page
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Check all three variables Session1, Session2, Cookie. If all the three are not null then proceed further
+            if (Session["userLoggedin"] != null && Session["AuthToken"] != null
+                          && Request.Cookies["AuthToken"] != null)
+            {
+                //Second Check, if Cookie we created has the same value as Second Session we've created
+                if ((Session["AuthToken"].ToString().Equals(
+                           Request.Cookies["AuthToken"].Value)))
+                {
+                    lblMessage.ForeColor = System.Drawing.Color.Green;
+                    lblMessage.Font.Size = FontUnit.Point(15);
+                    lblMessage.Text = "Welcome " + Session["userLoggedin"].ToString();
+                    btnLogout.Visible = true;
+                    string cookieValue = "";
+                    cookieValue = Request.Cookies["AuthToken"].Value;
+                    lblAuthCookie.ForeColor = System.Drawing.Color.Green;
+                    lblAuthCookie.Font.Size = FontUnit.Point(15);
+                    lblAuthCookie.Text = "You AuthToken is " + cookieValue;
+                }
+                else
+                {
+                    Response.Redirect("~/SecureLoginFunc/SecureLogin.aspx");
+
+                }
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            //Clear Session
+            Session.Clear();
+            Session.Abandon();
+            Session.RemoveAll();
+            if (Request.Cookies["ASP.NET_SessionId"] != null)
+            {
+                //Empty the Cookie 
+                Response.Cookies["ASP.NET_SessionId"].Value = string.Empty;
+                Response.Cookies["ASP.NET_SessionId"].Expires = DateTime.Now.AddMonths(-20);
+            }
+            if (Request.Cookies["AuthToken"] != null)
+            {
+                //Empty the Cookie 
+                Response.Cookies["AuthToken"].Value = string.Empty;
+                Response.Cookies["AuthToken"].Expires = DateTime.Now.AddMonths(-20);
+            }
+            Response.Redirect("~/SecureLoginFunc/SecureLogin.aspx");
+        }
+    }
+}
